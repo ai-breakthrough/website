5cb81c70d545cc442b661ae0ca38a045abb2a78e
Vitalii Bozadzhy <VitaliiBozadzhy@icloud.com>
2025-06-27 13:37:35 +0200

    init

---
+++ src/test/java/dk/aibreakthrough/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.aibreakthrough.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
